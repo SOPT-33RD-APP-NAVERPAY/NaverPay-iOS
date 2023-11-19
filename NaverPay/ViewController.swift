aa9c09b46622c888023cb18661476219e4ad0cb5
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2023-11-19 23:39:39 +0900

    [Add] .gitignore 추가

---
+++ NaverPay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NaverPay
+//
+//  Created by 곽성준 on 11/19/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
