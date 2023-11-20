6f0a50d7e8ef5220dd3a3090b8efdb87ad55104d
jaminleee <jamin_00@naver.com>
2023-11-20 23:37:56 +0900

    [Add] PlaceViewController 생성

---
+++ NaverPay/PlaceViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PlaceViewController.swift
+//  NaverPay
+//
+//  Created by 이자민 on 2023/11/20.
+//
+
+import UIKit
+
+class PlaceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
