4e72d0acd4f6a8877b02ef2f6cc0491a31e4408d
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2023-11-21 20:27:19 +0900

    [Feat] benefitCollectionView 생성 및 설정(#12)

---
+++ NaverPay/BenefitViewController.swift
@@ -0,0 +1,85 @@
+//
+//  BenefitViewController.swift
+//  NaverPay
+//
+//  Created by 곽성준 on 11/21/23.
+//
+
+import UIKit
+import SnapKit
+
+final class BenefitViewController: UIViewController {
+
+    
+    private let benefitCollectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: setCollectionViewLayout())
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.backgroundColor = .bg_gray
+        return collectionView
+    }()
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setLayout()
+        setCollectionView()
+    }
+    
+    
+    
+    private func setLayout() {
+        view.addSubviews(benefitCollectionView)
+        
+        benefitCollectionView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.trailing.leading.bottom.equalToSuperview()
+        }
+        
+    }
+    
+    private func setCollectionView() {
+        benefitCollectionView.dataSource = self
+        benefitCollectionView.delegate = self
+    }
+    
+    static func setCollectionViewLayout() -> UICollectionViewLayout {
+        
+        let layout = UICollectionViewCompositionalLayout { (sectionNumber, env) -> NSCollectionLayoutSection? in
+            
+            switch sectionNumber {
+            case 0:
+                let item = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .estimated(280)))
+                let group = NSCollectionLayoutGroup.vertical(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .estimated(1.5)), subitems: [item])
+                let section = NSCollectionLayoutSection(group: group)
+                section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 44, trailing: 0)
+                
+                return section
+                
+            default:
+                return nil
+            }
+        }
+        return layout
+    }
+}
+
+extension BenefitViewController: UICollectionViewDelegate { }
+extension BenefitViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    
+}
