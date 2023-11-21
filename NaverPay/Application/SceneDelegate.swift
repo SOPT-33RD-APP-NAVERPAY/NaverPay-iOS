84e3f31636e2b96d23e2ba663b508a15600fd6df
jaminleee <jamin_00@naver.com>
2023-11-22 00:21:20 +0900

    [Chore] 파일 삭제 및 이동 (#8)

--- NaverPay/Application/SceneDelegate.swift
+++ NaverPay/Application/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
       guard let windowScene = (scene as? UIWindowScene) else { return }
       self.window = UIWindow(windowScene: windowScene)
-      let navigationController =  ViewController()
+      let navigationController =  PlaceViewController()
       self.window?.rootViewController = navigationController
       self.window?.makeKeyAndVisible()
   }
