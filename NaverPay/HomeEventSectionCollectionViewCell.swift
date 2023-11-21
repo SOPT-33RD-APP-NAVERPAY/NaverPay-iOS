d04a7c5a74b89824c01c2e3f238036f2c87a3b88
kim-seonwoo <sw204011@gmail.com>
2023-11-22 00:42:31 +0900

    [Add] identifier가 추가된 HomeView 관련 파일 추가 (#15)

---
+++ NaverPay/HomeEventSectionCollectionViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  MainEventSectionCollectionViewCell.swift
+//  NaverPay
+//
+//  Created by Seonwoo Kim on 2023/11/21.
+//
+
+import UIKit
+
+class HomeEventSectionCollectionViewCell: UICollectionViewCell {
+    static let identifier: String = "HomeEventSectionCollectionViewCell"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        // 랜덤으로 배경색 지정
+        self.backgroundColor = UIColor(
+            red: drand48(),
+            green: drand48(),
+            blue: drand48(),
+            alpha: drand48()
+        )
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
