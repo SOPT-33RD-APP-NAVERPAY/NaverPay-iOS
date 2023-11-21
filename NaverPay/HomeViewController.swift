1c4905aa60b715c7b735717175035f6ca870b3a3
kim-seonwoo <sw204011@gmail.com>
2023-11-21 21:30:57 +0900

    [Feat] HomeViewController CollectionView 기초 형식 설정 추가

--- NaverPay/HomeViewController.swift
+++ NaverPay/HomeViewController.swift
@@ -6,24 +6,79 @@
 //
 
 import UIKit
+import SnapKit
 
 class HomeViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    private let HomeCollectionView: UICollectionView = {
+          let collectionView = UICollectionView(frame: .zero, collectionViewLayout: setCollectionViewLayout())
+          collectionView.showsVerticalScrollIndicator = false
+          collectionView.showsHorizontalScrollIndicator = false
+        collectionView.backgroundColor = .bg_black
+          return collectionView
+      }()
 
-        // Do any additional setup after loading the view.
-    }
-    
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+      override func viewDidLoad() {
+          super.viewDidLoad()
+
+          setLayout()
+          setCollectionView()
+      }
+
+
+
+      private func setLayout() {
+          view.addSubviews(HomeCollectionView)
+
+          HomeCollectionView.snp.makeConstraints {
+              $0.top.equalTo(view.safeAreaLayoutGuide)
+              $0.trailing.leading.bottom.equalToSuperview()
+          }
+
+      }
+
+      private func setCollectionView() {
+          HomeCollectionView.dataSource = self
+          HomeCollectionView.delegate = self
+      }
+
+      static func setCollectionViewLayout() -> UICollectionViewLayout {
+
+          let layout = UICollectionViewCompositionalLayout { (sectionNumber, env) -> NSCollectionLayoutSection? in
+
+              switch sectionNumber {
+              case 0:
+                  let item = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .estimated(280)))
+                  let group = NSCollectionLayoutGroup.vertical(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .estimated(1.5)), subitems: [item])
+                  let section = NSCollectionLayoutSection(group: group)
+                  section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 44, trailing: 0)
+
+                  return section
+
+              default:
+                  return nil
+              }
+          }
+          return layout
+      }
+  }
+
+  extension HomeViewController: UICollectionViewDelegate { }
+  extension HomeViewController: UICollectionViewDataSource {
+
+      func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+          return 1
+      }
+
+      func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+          return UICollectionViewCell()
+      }
+
+      func numberOfSections(in collectionView: UICollectionView) -> Int {
+          return 1
+      }
+
 
 }
