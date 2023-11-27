644e2ac1019b9eabeb7890885a7e72fb6acdf989
jaminleee <jamin_00@naver.com>
2023-11-27 20:54:33 +0900

    [Feat] PieChartView 생성

---
+++ NaverPay/Scenes/Point/Views/PieChartView.swift
@@ -0,0 +1,20 @@
+//
+//  PieChartView.swift
+//  NaverPay
+//
+//  Created by 이자민 on 2023/11/27.
+//
+
+import UIKit
+
+class PieChartView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
