4275969fc41b05db66e45c1c7e9f495b24b3d8b3
jaminleee <jamin_00@naver.com>
2023-11-27 20:06:46 +0900

    [Feat] 이미지 파일 추가 및 데이터 파일 생성

---
+++ NaverPay/Scenes/Place/Models/UserPlaceAppData.swift
@@ -0,0 +1,8 @@
+//
+//  UserPlaceAppData.swift
+//  NaverPay
+//
+//  Created by 이자민 on 2023/11/25.
+//
+
+import Foundation
---
+++ NaverPay/Scenes/Point/Models/UserPointAppData.swift
@@ -0,0 +1,8 @@
+//
+//  UserPointAppData.swift
+//  NaverPay
+//
+//  Created by 이자민 on 2023/11/25.
+//
+
+import Foundation
