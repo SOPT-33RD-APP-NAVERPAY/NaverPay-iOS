5cddd85b9b0ecf255a8f0af3a9eaf25f2bccedb7
jaminleee <jamin_00@naver.com>
2023-11-28 22:11:06 +0900

    [Feat] Point, Date, Time 데이터 가공

--- NaverPay/Scenes/Point/Models/UserPointAppData.swift
+++ NaverPay/Scenes/Point/Models/UserPointAppData.swift
@@ -14,9 +14,35 @@ import Foundation
 
 // MARK: - DataClass
 struct UserPointAppData {
-    let userPoint, basicPoint, membershipPoint, reviewPoint: Int
-    let paymentMethodPoint: Int
+    let paymentMethodPoint, basicPoint, membershipPoint, reviewPoint: Int
+    let userPoint: Int
     let brandList: [PointBrandList]
+    
+    var formattedPaymentPoint: String {
+        return formatNumber(paymentMethodPoint)
+    }
+    
+    var formattedUserPoint: String {
+        return formatNumber(userPoint)
+    }
+    
+    var formattedBasicPoint: String {
+        return formatNumber(basicPoint)
+    }
+    
+    var formattedMembershipPoint: String {
+        return formatNumber(membershipPoint)
+    }
+    
+    var formattedReviewPoint: String {
+        return formatNumber(reviewPoint)
+    }
+    
+    private func formatNumber(_ number: Int) -> String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        return numberFormatter.string(from: NSNumber(value: number)) ?? ""
+    }
 }
 
 // MARK: - BrandList
@@ -25,17 +51,65 @@ struct PointBrandList {
     let name: String
     let saving: Int
     let paymentDate: String
+    
+    var formattedSaving: String {
+        return formatNumber(saving)
+    }
+    
+    var formattedPaymentDate: String {
+        return formateDate(paymentDate)
+    }
+    
+    var formattedPaymentTime: String {
+        return formateTime(paymentDate)
+    }
+    
+    private func formateDate(_ string: String) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+        
+        if let date = formatter.date(from: string) {
+            formatter.dateFormat = "MM.dd"
+            return formatter.string(from: date)
+        }
+        else {
+            return "Invalid Date"
+        }
+        
+    }
+    
+    private func formateTime(_ string: String) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+        
+        if let time = formatter.date(from: string) {
+            formatter.dateFormat = "HH:mm"
+            return formatter.string(from: time)
+        }
+        else {
+            return "Invalid Time"
+        }
+        
+    }
+    
+    
+    private func formatNumber(_ number: Int) -> String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        return numberFormatter.string(from: NSNumber(value: number)) ?? ""
+    }
 }
 
 
 extension UserPointAppData {
     static func dummy() -> UserPointAppData {
         
-        UserPointAppData(userPoint: 1000, basicPoint: 2000, membershipPoint: 3000, reviewPoint: 4000, paymentMethodPoint: 5000, brandList: [
-            PointBrandList(id: 1, name: "가게 이름1", saving: 1000, paymentDate: "11:15"),
-            PointBrandList(id: 2, name: "가게 이름2", saving: 2000, paymentDate: "12:15"),
-            PointBrandList(id: 3, name: "가게 이름3", saving: 3000, paymentDate: "13:15"),
-            PointBrandList(id: 4, name: "가게 이름4", saving: 4000, paymentDate: "14:15")
+        UserPointAppData(paymentMethodPoint: 1000, basicPoint: 2000, membershipPoint: 3000, reviewPoint: 4000, userPoint: 5000, brandList: [
+            PointBrandList(id: 1, name: "가게 이름1", saving: 1000, paymentDate: "2023-11-27T13:18:59"),
+            PointBrandList(id: 2, name: "가게 이름2", saving: 2000, paymentDate: "2023-11-27T13:18:59"),
+            PointBrandList(id: 3, name: "가게 이름3", saving: 3000, paymentDate: "2023-11-27T13:18:59"),
+            PointBrandList(id: 4, name: "가게 이름4", saving: 4000, paymentDate: "2023-11-27T13:18:59"),
+            
         ])
         
     }
