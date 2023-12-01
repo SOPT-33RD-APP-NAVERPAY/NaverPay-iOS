19626145e881498adcc3fb814d6f0537ba1e8d1b
kim-seonwoo <sw204011@gmail.com>
2023-12-01 14:14:30 +0900

    [Chore] 그라데이션 색상 수정 (#52)

--- NaverPay/Scenes/Home/Views/HomePointSectionBackgroundView.swift
+++ NaverPay/Scenes/Home/Views/HomePointSectionBackgroundView.swift
@@ -26,29 +26,24 @@ final class HomePointSectionBackgroundView: UICollectionReusableView {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    //그라데이션을 구현
+ 
     private func setGradientBackground() {
         let gradientLayer = CAGradientLayer()
-        backgroundView.layer.cornerRadius = 10
-        backgroundView.clipsToBounds = true
-        
-        self.layer.cornerRadius = 10
-        self.clipsToBounds = true
-        
-        
+
         gradientLayer.colors = [
-            UIColor(red: 0.02, green: 0.67, blue: 0.40, alpha: 1.0).cgColor, // #06AA65
-            UIColor(red: 0.03, green: 0.67, blue: 0.55, alpha: 1.0).cgColor  // #07AA8C
+            UIColor(red: 0.03, green: 0.67, blue: 0.55, alpha: 1.00).cgColor,  // #07AA8C
+            UIColor(red: 0.02, green: 0.67, blue: 0.40, alpha: 1.00).cgColor  // #06AA65
         ]
-        
+
         let angle = 113.0 * .pi / 180.0
         let x = cos(angle)
         let y = sin(angle)
-        
-        gradientLayer.startPoint = CGPoint(x: CGFloat(x), y: CGFloat(y))
+
+        gradientLayer.startPoint = CGPoint(x: 1.0, y: 0.0)
         gradientLayer.endPoint = CGPoint(x: 0.0, y: 1.0)
         gradientLayer.frame = bounds
-        
+        gradientLayer.locations = [0.0232, 0.9345]
+
         layer.insertSublayer(gradientLayer, at: 0)
     }
     
